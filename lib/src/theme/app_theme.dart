6f3f8d9faea0bd0be64c3144893de7b305dc2abf
José Lucas da Silva <joselucas0303@gmail.com>
2022-10-12 17:35:08 -0300

    chore: add constant text colors

--- lib/src/theme/app_theme.dart
+++ lib/src/theme/app_theme.dart
@@ -4,4 +4,7 @@ class AppTheme {
   static const Color primaryColor = Colors.white;
   static const Color secondaryColor = Color(0XFF2B406F);
   static const Color backgroundColor = Color(0XFFFCFCFD);
+
+  static const Color headlineColor = Color(0XFF131631);
+  static final Color textColor = Colors.grey.shade700;
 }
