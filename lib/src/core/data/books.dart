62d44052aacf9c470497fc51f0c05aa79cd95302
José Lucas da Silva <joselucas0303@gmail.com>
2022-10-12 22:27:52 -0300

    refactor: add missing props to book_model

--- lib/src/core/data/books.dart
+++ lib/src/core/data/books.dart
@@ -1,43 +1,52 @@
+import '../enums/gender_enum.dart';
 import '../models/book_model.dart';
 
 const _kSynopsis = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae libero eu ipsum cursus egestas in at purus. Mauris tortor tortor, tincidunt ut nisi sit amet, cursus pretium turpis. Curabitur interdum, nisi at condimentum hendrerit, sapien lacus congue nisi, id eleifend tellus nisi nec nibh. Cras blandit hendrerit nulla quis vehicula. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus tincidunt pulvinar lobortis. Duis condimentum leo ut neque gravida ultrices. Curabitur placerat felis ultrices, accumsan libero laoreet, vestibulum ex. Vivamus tempus nisl quis nunc bibendum, at tincidunt est mollis. Sed libero arcu, hendrerit at nulla vitae, sagittis tincidunt urna. Vivamus non sollicitudin felis. Integer sem metus, mattis et lacus et, accumsan dapibus nunc. Nulla facilisi. Integer id vehicula sapien.';
 
 const kBooks = <BookModel>[
   BookModel(
+    id: 1,
     title: 'Ther Melian: Disocrd',
     author: 'Shienny M.S',
     image: 'assets/images/1.jpg',
     synopsis: _kSynopsis,
+    gender: Gender.fantasy,
     rate: 4.8,
     pages: 340,
     totalReadTimes: 4200,
     read: false,
   ),
   BookModel(
+    id: 2,
     title: 'The Poppy War',
     author: 'R.F Kuang',
     image: 'assets/images/2.jpg',
     synopsis: _kSynopsis,
+    gender: Gender.adventure,
     rate: 4.5,
     pages: 124,
     totalReadTimes: 1234,
     read: true,
   ),
   BookModel(
+    id: 3,
     title: 'The Glass Magical',
     author: 'Charlie F.M',
     image: 'assets/images/3.jpg',
     synopsis: _kSynopsis,
+    gender: Gender.romance,
     rate: 4.0,
     pages: 340,
     totalReadTimes: 1324,
     read: true,
   ),
   BookModel(
+    id: 4,
     title: 'Pergi',
     author: 'Tere Liye',
     image: 'assets/images/4.jpg',
     synopsis: _kSynopsis,
+    gender: Gender.mistery,
     rate: 2.4,
     pages: 149,
     totalReadTimes: 24,
--- lib/src/core/models/book_model.dart
+++ lib/src/core/models/book_model.dart
@@ -1,19 +1,25 @@
+import '../enums/gender_enum.dart';
+
 class BookModel {
   const BookModel({
+    required this.id,
     required this.title,
     required this.author,
     required this.image,
     required this.synopsis,
+    required this.gender,
     required this.rate,
     required this.pages,
     required this.totalReadTimes,
     required this.read,
   });
 
+  final int id;
   final String title;
   final String author;
   final String image;
   final String synopsis;
+  final Gender gender;
   final double rate;
   final int pages;
   final int totalReadTimes;
